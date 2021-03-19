9ddfd61e872a761c8fb1e09ba077368c0ea47fb1
fistadev <fabiofistarol@gmail.com>
2021-03-19 18:55:35 +0100

    Create number_37.py

---
+++ number_37.py
@@ -0,0 +1,13 @@
+# number 37 is magical
+a = int(input('Number: '))
+n = 37
+
+result = n * (3 * a)
+check_result = 3 * str(a)
+
+print('(', a, '+', a, '+', a, ') ', '* 37 =', result)
+prin
+t(a, ',', a, ',', a, ': ', check_result)
+
+print(f'( {a} + {a} + {a} ) * 37 = {result}')
+print(f'{a} , {a} , {a} : {check_result}')
