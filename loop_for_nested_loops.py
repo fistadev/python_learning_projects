16ea268ada9733f60526de0f4fd3f6ef7a03518f
fistadev <fabiofistarol@gmail.com>
2021-03-19 09:48:33 +0100

    First commit of this repo

---
+++ loop_for_nested_loops.py
@@ -0,0 +1,7 @@
+# print letter F with '#' with foo loop
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ""
+    for count in range(x_count):
+        output += "#"
+    print(output)
