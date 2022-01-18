c2c641afa2b93484e775ab7491461ac32dadc2c3
fistadev <fabiofistarol@gmail.com>
2022-01-18 11:33:10 +0100

    Update location

---
+++ math/factorial.py
@@ -0,0 +1,13 @@
+# factorial
+
+def fact(n):
+    f = 1
+    for i in range(1, n+1):
+        f *= i
+    return f
+
+
+a = int(input('Write a number: '))
+
+result = fact(a)
+print('Factorial is: ', result)
